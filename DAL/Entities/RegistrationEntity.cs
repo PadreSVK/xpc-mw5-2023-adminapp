50c0e681923196a60e9a28109c95fc5e4fffc8b4
pepsa121 <josefotevrel@seznam.cz>
2023-03-18 22:09:57 +0100

    update entities

--- DAL/Entities/RegistrationEntity.cs
+++ DAL/Entities/RegistrationEntity.cs
@@ -7,9 +7,9 @@ public record RegistrationEntity : IEntity
     public required Guid Id { get; set; }
     public required string FirstName { get; set;}
     public required string LastName { get; set;}
-    public required EmailEntity EmailAddress { get; set; }
+    public EmailEntity EmailAddress { get; set; }
     public required string PhoneNumber { get; set; }
-    public required DepartmentEntity Department { get; set; }
+    public DepartmentEntity Department { get; set; }
     public required DateTime BirthDate { get; set; }
     public required GenderEnum Gender { get; set; }
     public required TransportModeEnum TransportMode { get; set; }
