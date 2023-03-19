1476ac600fd7b1131789be09209456ef226fa353
matej-malysa <240391@vutbr.cz>
2023-03-19 10:05:28 -0700

    added IFacade, facadebase, blinstaller, businesslogic

---
+++ BLL/BusinessLogic.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AdminApp.BLL.Tests")]
+
+namespace AdminApp.BLL;
+
+internal class BusinessLogic
+{
+}
---
+++ BLL/Facades/Interfaces/IFacade.cs
@@ -0,0 +1,16 @@
+﻿using AdminApp.BLL.Models;
+using AdminApp.DAL.Entities;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AdminApp.BLL.Facades;
+
+public interface IFacade<TEntity, TListModel, TDetailModel>
+    where TEntity : class, IEntity
+    where TListModel : IModel
+    where TDetailModel : class, IModel
+{
+    Task DeleteAsync(Guid id);
+    Task<TDetailModel?> GetAsync(Guid id);
+    Task<IEnumerable<TListModel>> GetAsync();
+    Task<TDetailModel> SaveAsync(TDetailModel model);
+}
