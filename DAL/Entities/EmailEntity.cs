f0a7a3199dcd15f6c8ee5c212c5271b3568683c7
pepsa121 <josefotevrel@seznam.cz>
2023-03-19 18:51:21 +0100

    email

--- DAL/Entities/EmailEntity.cs
+++ DAL/Entities/EmailEntity.cs
@@ -4,7 +4,7 @@ public record EmailEntity : IEntity
 {
     public required Guid Id { get; set; }
     public required string Email { get; set; }
-
+    public required string Password { get; set; }
     public Guid RegistrationId { get; set; }
     public RegistrationEntity Registration { get; set; }
 }
